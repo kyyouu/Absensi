dd4a1ff8fc5dd291e7e2dc0aecb8add3fbab896c
NamaMu <email@example.com>
2026-08-12 23:15:53 +0700

    Configure Supabase credentials (URL & ANON_KEY)

--- js/config.js
+++ js/config.js
@@ -9,6 +9,6 @@ const SUPABASE_CONFIG = {
   // Project URL Supabase
   URL: 'https://ehbidyovspcdvhavwwvu.supabase.co',
 
-  // anon public key Supabase (masukkan anon key Anda di sini)
-  ANON_KEY: ''
+  // anon public key Supabase
+  ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoYmlkeW92c3BjZHZoYXZ3d3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1NDU0NzIsImV4cCI6MjEwMjEyMTQ3Mn0.sRFBT_ZFMgrTy6isfjjB9x18WGRGwhSGVtYDsAW3BHk'
 };
